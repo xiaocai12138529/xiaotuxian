6c870a6909d6601d83be44a2525f5b2405984340
xiaocai <1766607952@qq.com>
2021-07-14 12:05:26 +0800

    store: Vuex persistence

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,31 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <!-- <div id="nav">
+    是否苹果自由{{$store.getters.isAppleFree}}
+    现在:{{$store.state.salary}}
+    跳槽:{{$store.state.companyName}}
+    <button @click="add">加薪</button>
+    <button @click="$store.dispatch('jumpTo','baidu')">跳槽</button>
+  </div> -->
+  {{$store.state.user.profile}}
+  <button @click="$store.commit('user/setUser',{id:3,username:'小王'})">修改user模块,测试持久化调用</button>
+  <router-view />
 </template>
+
+<script>
+// 导入
+import { useStore } from 'vuex'
+export default ({
+  setup () {
+    // 调用
+    const store = useStore()
+    const add = () => {
+      // 去调用 mutations
+      store.commit('addSalary', 1000)
+    }
+    return { add }
+  }
+})
+</script>
 
 <style lang="less">
 #app {
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,55 @@
+// import Vuex from 'vuex'
+// import vue from 'vue'
+// Vue.use(Vuex)
+// export default new Vuex.Store({
+
+// })
 import { createStore } from 'vuex'
+import cart from './module/cart'
+import category from './module/category'
+import user from './module/user'
+import createPersistedstate from 'vuex-persistedstate'
 
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+  // // 定义数据
+  // state: {
+  //   name: '小王',
+  //   salary: 12000,
+  //   companyName: 'DIDI'
+  // },
+  // // 定义修改数据的方法
+  // mutations: {
+  //   // 涨薪
+  //   addSalary (state, num) {
+  //     state.salary += num
+  //   },
+  //   // 跳槽
+  //   changeCompany (state, num) {
+  //     state.companyName = num
+  //   }
+  // },
+  // // 监听
+  // getters: {
+  //   isAppleFree (state) {
+  //     return state.salary > 30000
+  //   }
+  // },
+  // actions: {
+  //   jumpTo (context, companyName = 'JD') {
+  //     setTimeout(() => {
+  //       context.commit('changeCompany', companyName)
+  //     }, 1000)
+  //   }
+  // },
   modules: {
-  }
+    cart,
+    category,
+    user
+  },
+  plugins: [
+    createPersistedstate({
+      key: 'erabbit-client-pc-store', // 起个名
+      paths: ['user', 'cart'] // 要做持久化的模块
+    })
+  ]
 })
