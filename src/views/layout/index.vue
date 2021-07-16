39577bc9bfd3b0c225bc75ecdae7bc114955feeb
xiaocai <1766607952@qq.com>
2021-07-16 11:47:56 +0800

    layout: Home page layout

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,20 +1,31 @@
 <template>
   <!-- 顶部导航 -->
-  <nav>1.顶部通栏</nav>
+  <!-- <nav>1.顶部通栏</nav> -->
+  <TopNav />
   <!-- 菜单分类 -->
-  <header>2.菜单-分类</header>
+  <!-- <header>2.菜单-分类</header> -->
+  <AppHeader />
   <!-- 主体 -->
   <main>
     <!-- 主体出口 -->
     <router-view />
   </main>
   <!-- 底部 -->
-  <footer>4.底部区域</footer>
+  <!-- <footer>4.底部区域</footer> -->
+  <Footer />
 </template>
 
 <script>
+import TopNav from './component/topnav'
+import AppHeader from './component/header.vue'
+import Footer from './component/footer.vue'
 export default {
-  name: 'Layout'
+  name: 'Layout',
+  components: {
+    TopNav,
+    AppHeader,
+    Footer
+  }
 }
 </script>
 
