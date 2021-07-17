bc30173570419ced3285261edf67ffcd197baef4
xiaocai <1766607952@qq.com>
2021-07-17 16:17:02 +0800

    home: xtxMore module

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,12 @@
 import XtxSkeleton from './xtx-skeleton.vue'
+import XtxMore from './xtx-more.vue'
 import XtxCarousel from '@/components/xtx-carousel.vue'
 
 const MyPlugin = {
   install (app) {
     app.component(XtxSkeleton.name, XtxSkeleton)
     app.component(XtxCarousel.name, XtxCarousel)
+    app.component(XtxMore.name, XtxMore)
   }
 }
 
