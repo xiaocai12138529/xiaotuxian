23b9c0f1a593e549b952d138f858f8de0e77805d
xiaocai <1766607952@qq.com>
2021-07-17 09:33:07 +0800

    home: left classify data  rendering

---
+++ src/views/home/components/category.vue
@@ -0,0 +1,172 @@
+<template>
+  <div class='home-category'>
+    <ul class="menu">
+      <li
+        v-for="item in cateList"
+        :key="item.id"
+      >
+        <RouterLink to="/">{{item.name}}</RouterLink>
+        <template v-if="item.children">
+          <RouterLink
+            to="/"
+            v-for="i in item.children"
+            :key="i.id"
+          >{{i.name}}</RouterLink>
+          <!-- <RouterLink to="/">二级类目</RouterLink> -->
+        </template>
+      </li>
+    </ul>
+    <!-- 弹层 -->
+    <div class="layer">
+      <h4>分类推荐 <small>根据您的购买或浏览记录推荐</small></h4>
+      <ul>
+        <li
+          v-for="i in 9"
+          :key="i"
+        >
+          <RouterLink to="/">
+            <img
+              src="https://yanxuan-item.nosdn.127.net/5a115da8f2f6489d8c71925de69fe7b8.png"
+              alt=""
+            >
+            <div class="info">
+              <p class="name ellipsis-2">【定金购】严选零食大礼包（12件）</p>
+              <p class="desc ellipsis">超值组合装，满足馋嘴欲</p>
+              <p class="price"><i>¥</i>100.00</p>
+            </div>
+          </RouterLink>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import { computed, reactive } from 'vue'
+import { useStore } from 'vuex'
+export default {
+  name: 'HomeCategory',
+  setup () {
+    const store = useStore()
+    const brand = reactive({
+      id: '',
+      name: '品牌',
+      children: [{ id: '', name: '推荐品牌' }]
+    })
+    // 使用计算属性从vuex中获取分类
+    const cateList = computed(() => {
+      const list = store.state.category.list.map(item => {
+        return {
+          id: item.id,
+          name: item.name,
+          children: item.children && item.children.slice(0, 2)
+        }
+      })
+      list.push(brand)
+      return list
+    })
+    return { cateList }
+  }
+}
+</script>
+
+<style scoped lang='less'>
+.home-category {
+  width: 250px;
+  height: 500px;
+  background: rgba(0, 0, 0, 0.8);
+  position: relative;
+  z-index: 99;
+  .menu {
+    li {
+      padding-left: 40px;
+      height: 50px;
+      line-height: 50px;
+      &:hover {
+        background: @xtxColor;
+      }
+      a {
+        margin-right: 4px;
+        color: #fff;
+        &:first-child {
+          font-size: 16px;
+        }
+      }
+    }
+  }
+  // 省略其他...
+  .layer {
+    width: 990px;
+    height: 500px;
+    background: rgba(255, 255, 255, 0.8);
+    position: absolute;
+    left: 250px;
+    top: 0;
+    display: none;
+    padding: 0 15px;
+    h4 {
+      font-size: 20px;
+      font-weight: normal;
+      line-height: 80px;
+      small {
+        font-size: 16px;
+        color: #666;
+      }
+    }
+    ul {
+      display: flex;
+      flex-wrap: wrap;
+      li {
+        width: 310px;
+        height: 120px;
+        margin-right: 15px;
+        margin-bottom: 15px;
+        border: 1px solid #eee;
+        border-radius: 4px;
+        background: #fff;
+        &:nth-child(3n) {
+          margin-right: 0;
+        }
+        a {
+          display: flex;
+          width: 100%;
+          height: 100%;
+          align-items: center;
+          padding: 10px;
+          &:hover {
+            background: #e3f9f4;
+          }
+          img {
+            width: 95px;
+            height: 95px;
+          }
+          .info {
+            padding-left: 10px;
+            line-height: 24px;
+            width: 190px;
+            .name {
+              font-size: 16px;
+              color: #666;
+            }
+            .desc {
+              color: #999;
+            }
+            .price {
+              font-size: 22px;
+              color: @priceColor;
+              i {
+                font-size: 16px;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  &:hover {
+    .layer {
+      display: block;
+    }
+  }
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,10 +1,24 @@
 <template>
-  首页-轮播图
+  <div class="page-home">
+    <div class="home-entry">
+      <div class="container">
+        <!-- 左侧分类 -->
+        <HomeCategory />
+      </div>
+    </div>
+  </div>
+  <HomeBanner />
 </template>
 
 <script>
+import HomeBanner from './components/banner.vue'
+import HomeCategory from './components/category.vue'
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    HomeBanner,
+    HomeCategory
+  }
 
 }
 </script>
