c1ca95b287629746c9ff85cac21f90b1b08519c4
xiaocai <1766607952@qq.com>
2021-07-23 11:09:14 +0800

    produce: goods-images end

--- src/views/product/components/goods-image.vue
+++ src/views/product/components/goods-image.vue
@@ -1,10 +1,23 @@
 <template>
   <div class="goods-image">
-    <div class="middle">
+    <div
+      v-if="!isOutside"
+      class="large"
+      :style="{backgroundImage: `url(${images[curIdx]})`, ...largeStyle}"
+    ></div>
+    <div
+      class="middle"
+      ref="target"
+    >
       <img
         :src="images[curIdx]"
         alt=""
       >
+      <div
+        class="layer"
+        :style="layerStyle"
+        v-if="!isOutside"
+      ></div>
     </div>
     <ul class="small">
       <li
@@ -22,14 +35,31 @@
   </div>
 </template>
 <script>
-import { ref } from 'vue'
+import { reactive, ref, watch } from 'vue'
+import { useMouseInElement } from '@vueuse/core'
+
 export default {
   name: '',
   props: { images: { type: Array, default: () => ([]) } },
   setup () {
-    const curIdx = ref(0)
+    const curIdx = ref(0) // 显示图片的效果
+    const target = ref(null)
+    const layerStyle = reactive({ top: 0, left: 0 })
+    const largeStyle = reactive({})
 
-    return { curIdx }
+    const { elementX, elementY, isOutside } = useMouseInElement(target)
+    watch([elementX, elementY, isOutside], () => {
+      let top = elementY.value - 100
+      let left = elementX.value - 100
+      if (top > 200) top = 200
+      if (left > 200) left = 200
+      if (top < 0) top = 0
+      if (left < 0) left = 0
+      layerStyle.top = top + 'px'
+      layerStyle.left = left + 'px'
+      largeStyle['background-position'] = `-${top * 2}px -${left * 2}px`
+    })
+    return { curIdx, target, layerStyle, isOutside, largeStyle }
   }
 }
 </script>
@@ -39,10 +69,31 @@ export default {
   height: 400px;
   position: relative;
   display: flex;
+  z-index: 500;
+  .large {
+    position: absolute;
+    top: 0;
+    left: 412px;
+    width: 400px;
+    height: 400px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    background-repeat: no-repeat;
+    background-size: 800px 800px;
+    background-color: #f8f8f8;
+  }
   .middle {
     width: 400px;
     height: 400px;
-    background: #f5f5f5;
+    position: relative;
+    cursor: move;
+    .layer {
+      width: 200px;
+      height: 200px;
+      background: rgba(0, 0, 0, 0.2);
+      left: 0;
+      top: 0;
+      position: absolute;
+    }
   }
   .small {
     width: 80px;
